a6fa09e7b60d3c898fb89f0c888976ce680a31af
rheslip <rheslip@hotmail.com>
2019-03-19 22:38:15 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-# Motivation Radio - WiFiOSC
+### WiFi OSC Message Host the Motivation Radio Eurorack module
+
+Motivation Radio is a general purpose eurorack module based on the ESP32 with:
+
+* 4 Gate/Trigger inputs
+
+* 4 CV inputs -5V to +5V range, 12 bit resolution
+
+* 4 Gate/Trigger outputs 0-6V
+
+* 4 CV outputs 0-6.144V 12 bit resolution
+
+* 128x32 OLED display
+
+* Encoder with switch
+
+* MIDI in and out with 1/8 jacks - MIDI in works with either polarity (Korg or Arturia), MIDI out is MMA standard (Korg)
+
+* Bluetooth and Wifi are supported by the ESP32 module
+
+
+Motivation Radio is intended mainly for interfacing eurorack to other devices (e.g. iPad) wirelessly but it can do more!
+
+### BLEMIDI Hardware Usage
+
+* Gate inputs - currently unused in this sketch
+
+* CV inputs - currently unused in this sketch
+
+* Gate outputs - controlled by OSC messages
+
+* CV outputs - voltages set by OSC messages
+
+
+Serial MIDI in - currently unused in this sketch
+
+Serial MIDI out - currently unused in this sketch
+
+Encoder and Button currently unused in this sketch
+
+
+
+### How to use WiFiOSC
+
+This sketch was written for and tested with the iOS app TouchOSC https://hexler.net/software/touchosc but it should work with anything that sends OSC messages in the format described below.
+ 
+The TouchOSC app allows you to create custom iPad UIs with sliders, buttons, switches, X-Y controllers etc. When you use the UI widgets TouchOSC sends an OSC message to a preconfigured IP address and port. This sketch will convert OSC messages to Gate signals and CV outputs on Movitation Radio. Its possible to send OSC messages from Motivation Radio to TouchOSC but this requires knowing the IP address of the iPad. There is probably some easy way to do this (Bonjour?) but its sending OSC messages is not implemented at this point.
+ 
+You will have to change the ssid and password in the sketch to match your WiFi network. Expressif has an API to set up SSID and password using an external app but I have not had much luck with it. 
+ 
+When Motivation Radio connects to the WiFi network via DHCP it will display its local IP on the OLED display. You have to enter this IP in the TouchOSC app so it knows where to send OSC messages. This sketch expects messages on port 8888 so you will have to specify that in TouchOSC as well. Each of the UI "widgets" can send an OSC message which must be in the format described below. Please read the TouchOSC documentation and learn to use the TouchOSC editor!
+ 
+To set Gate Out A (leftmost on the Motivation Radio panel) low, send the OSC message: 
+ 
+* /GateoutA,0 
+
+Note: the number should be sent as a float but is treated as boolean 0 or 1. To set Gate Out A high send the OSC message:
+ 
+* /GateoutA,1 
+ 
+The same message format is used  for Gate outs B, C and D
+
+ OSC CV messages- to set CV Out A to its lowest voltage (approx 0V) send the OSC message:
+ 
+* /CVoutA,0    
+ 
+The number should be sent as a float ranging from 0 (approx 0V out) to 1 (approx 6.144V out). This is intended to be used with sliders, X-Y pads etc.
+ 
+The Same message format is used for CV outs B, C and D
+
+Incoming OSC messages are NOT checked for valid range so be careful what you send or you may not get what you expect out.
+ 
+A very simple UI for TouchOSC is included in /resources. Four buttons control gates A-D and four sliders adjust the CV outputs A-D. You will have to load this in the TouchOSC editor on a PC and sync it to the TouchOSC app on your iPad.
+
+
+### Software Dependendencies:
+
+* Adafruit Graphics library and the SSD1306 driver https://github.com/adafruit/Adafruit-GFX-Library https://github.com/adafruit/Adafruit_SSD1306
+
+* OSC for Arduino https://github.com/CNMAT/OSC
+
+
+compiled with Arduino 1.85 with ESP32 Arduino installed. Use "ESP32 DEV Module" as the board. 
+
+More info in the Wiki: https://github.com/rheslip/Motivation-Radio---WiFiOSC/wiki
+
+Rich Heslip March 2019
+
---
+++ WiFiOSC.ino
@@ -0,0 +1,205 @@
+/*
+ WiFi OSC message host for Motivation Radio Eurorack ESP32 module
+ R Heslip March 2019 
+
+ This sketch was written for and tested with the iOS app TouchOSC but it should work with anything that sends OSC messages in the format described below.
+ 
+ The TouchOSC app allows you to create custom iPad UIs with sliders, buttons, switches, X-Y controllers etc. When you use the UI widgets TouchOSC sends
+ an OSC message to a preconfigured IP address and port. This sketch will convert OSC messages to Gate signals and CV outputs on Movitation Radio. Its
+ possible to send OSC messages from Motivation Radio to TouchOSC but this requires knowing the IP address of the iPad. There is probably some easy way to do this
+ (Bonjour?) but its sending OSC messages is not implemented at this point.
+ 
+ You will have to change the ssid and password in the sketch (below) to match your WiFi network. Expressif has an API to set up SSID and password with an external app but I have not
+ had much luck with it. 
+ 
+ When Motivation Radio connects to the WiFi network via DHCP it will display its local IP on the OLED display. You have to enter this IP in the TouchOSC app
+ so it knows where to send OSC messages. This sketch expects messages on port 8888 so you will have to specify that in TouchOSC as well. 
+ Each of the UI "widgets" can send an OSC message which must be in the format described below. Please read the TouchOSC documentation and learn to use the TouchOSC editor!
+ 
+ To set GateA low, send the OSC message: 
+ 
+ GateoutA,0 
+
+ Note: the number should be sent as a float but is treated as boolean 0 or 1. To set GateA high send the OSC message:
+ 
+ GateoutA,1 
+ 
+ Same message format is used  for GateoutB-D
+
+ OSC CV messages- to set CVoutA to its lowest voltage (approx 0V) send the OSC message:
+ 
+ CVoutA,0    
+ 
+ The number should be sent as a float ranging from 0 (approx 0V out) to 1 (approx 6.144V out). This is intended to be used with sliders, X-Y pads etc.
+ 
+ Same message format is used for CVoutB-D
+ Incoming OSC messages are NOT checked for valid range so be careful what you send or you may not get what you expect out.
+ 
+ History:
+ March 19/19 - ported code from proto HW to Motivation Radio HW and made more consistent with the other Motivation Radio sketches.                              
+*/
+
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+#include <OSCBundle.h>
+#include <OSCData.h>
+#include <Wire.h>
+#include "SPI.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "MORAD_IO.h"
+#include "DAC.H"
+#include "MORAD_utility.h"
+
+#define OLED_RESET -1  // to keep the compiler happy
+Adafruit_SSD1306 display(OLED_RESET);
+
+char ssid[] = "cruzer";          // your network SSID (name)
+char pass[] = "xa_bY+9939'zz";                    // your network password
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP Udp;
+const IPAddress outIp(10,40,10,105);        // remote IP (not needed for receive)
+const unsigned int outPort = 9999;          // remote port (not needed for receive)
+const unsigned int localPort = 8888;        // local port to listen for UDP packets (here's where we send the packets)
+
+
+OSCErrorCode errorcode;
+
+
+void setup() {
+
+  Serial.begin(115200);
+  
+  pinMode(DAC0_CS, OUTPUT);
+  pinMode(DAC1_CS, OUTPUT);
+  pinMode(ADC_CS, OUTPUT);  
+  digitalWrite(DAC0_CS, HIGH);
+  digitalWrite(DAC1_CS, HIGH);
+  digitalWrite(ADC_CS, HIGH);
+  pinMode(GATEout_0, OUTPUT);
+  pinMode(GATEout_1, OUTPUT);
+  pinMode(GATEout_2, OUTPUT);
+  pinMode(GATEout_3, OUTPUT);
+  
+  pinMode(GATEin_0, INPUT);
+  pinMode(GATEin_0, INPUT);
+  pinMode(GATEin_0, INPUT);
+  pinMode(GATEin_0, INPUT);
+
+  SPI.begin(SCLK,MISO,MOSI,DAC0_CS); // we actually use a CS pin for each DAC
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setFrequency(2000000); // ADC max clock 2 Mhz 
+    
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.clearDisplay();
+  display.println("   MORAD WiFiOSC");
+//  display.println("    Mar 19/2019");
+  display.println();
+  display.display();
+  delay(5000);
+    
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  //display.print("  IP: ");
+  display.print(WiFi.localIP());
+  
+  Serial.println("Starting UDP");
+  Udp.begin(localPort);
+  Serial.print("Local port: ");
+  display.print(":");
+#ifdef ESP32
+  Serial.println(localPort);
+  display.print(localPort);
+#else
+  Serial.println(Udp.localPort());
+#endif
+  display.display();
+}
+
+
+// Gate output message handlers - TouchOSC push buttons
+
+void gateoutA(OSCMessage &msg) {
+  float val = msg.getFloat(0); // values 0 to 1
+  GATEout(0,(bool)val);
+}
+
+void gateoutB(OSCMessage &msg) {
+  float val = msg.getFloat(0); // values 0 to 1
+  GATEout(1,(bool)val);
+}
+
+void gateoutC(OSCMessage &msg) {
+  float val = msg.getFloat(0); // values 0 to 1
+  GATEout(2,(bool)val);
+}
+
+void gateoutD(OSCMessage &msg) {
+  float val = msg.getFloat(0); // values 0 to 1
+  GATEout(3,(bool)val);
+}
+
+
+void CVoutA(OSCMessage &msg) {
+  float val = msg.getFloat(0); // values 0 to 1
+  CVout(0,(unsigned)(val*(DAC_RANGE-1)));
+}
+
+void CVoutB(OSCMessage &msg) {
+  float val = msg.getFloat(0); // values 0 to 1
+  CVout(1,(unsigned)(val*(DAC_RANGE-1)));
+}
+
+void CVoutC(OSCMessage &msg) {
+  float val = msg.getFloat(0); // values 0 to 1
+  CVout(2,(unsigned)(val*(DAC_RANGE-1)));
+}
+
+void CVoutD(OSCMessage &msg) {
+  float val = msg.getFloat(0); // values 0 to 1
+  CVout(3,(unsigned)(val*(DAC_RANGE-1)));
+}
+
+void loop() {
+  OSCMessage msg;
+  int size = Udp.parsePacket();
+
+  if (size > 0) {
+    while (size--) {
+      msg.fill(Udp.read());
+    }
+    if (!msg.hasError()) {
+      msg.dispatch("/GateA", gateoutA);
+      msg.dispatch("/GateB", gateoutB);
+      msg.dispatch("/GateC", gateoutC);
+      msg.dispatch("/GateD", gateoutD);
+      msg.dispatch("/CVoutA", CVoutA);
+      msg.dispatch("/CVoutB", CVoutB);
+      msg.dispatch("/CVoutC", CVoutC);
+      msg.dispatch("/CVoutD", CVoutD);
+    } else {
+      errorcode = msg.getError();
+      Serial.print("error: ");
+      Serial.println(errorcode);
+    }
+  }
+}
